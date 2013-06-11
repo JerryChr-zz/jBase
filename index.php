31f6d4116ca3266a09f362566e1f4dc331714759
JerryChr <jerri@redweb.dk>
2013-06-11 16:18:19 +0200

    redBASE

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+?>
+<?php echo '<?xml version="1.0" encoding="utf-8"?'.'>'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+<head>
+<!--
+	##########################################
+	## redWeb.dk                            ##
+	## Blangstedgaardsvej 1, 1 sal          ##
+	## 5220 Odense SØ                       ##
+	## Danmark                              ##
+	## email@redweb.dk                      ##
+	## http://www.redweb.dk                 ##
+	## Dato: 2013-06-11                     ##
+	##########################################
+-->
+	<?php $this->setMetaData('generator','redWEB ApS – www.redweb.dk'); ?>
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/normalize.css" />
+	<link rel="stylesheet/less" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/less/template.less" />
+    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/less.js" type="text/javascript"></script>
+
+<!--START Live LESS watcher / ONLY FOR DEVELOPMENT-->
+<script type="text/javascript">
+    less.watch();
+</script>
+<!--STOP Live LESS watcher / ONLY FOR DEVELOPMENT-->
+
+    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/modernizr.min.js" type="text/javascript"></script>
+    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/scripts.js" type="text/javascript"></script>
+
+</head>
+<body>
+
+<div id="wrapper">
+
+    <div class="hero-unit">
+        <h1>Heading</h1>
+        <p>Tagline</p>
+        <p>
+            <a class="btn btn-primary btn-large">
+                Learn more
+            </a>
+        </p>
+    </div>
+
+</div>
+
+
+</body>
+</html>
