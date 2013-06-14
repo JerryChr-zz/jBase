065f4e71f3f7d72b4d99a4ff2594d94360a59ae6
JerryChr <jerri@redweb.dk>
2013-06-14 11:12:40 +0200

    redBASE

--- index.php
+++ index.php
@@ -23,16 +23,10 @@ defined('_JEXEC') or die('Restricted access');
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/normalize.css" />
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/magic.css" />
 	<link rel="stylesheet/less" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/less/template.less" />
-    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/less.js" type="text/javascript"></script>
-    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery-1.9.1.min.js" type="text/javascript"></script>
-    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/modernizr.min.js" type="text/javascript"></script>
-    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/scripts.js" type="text/javascript"></script>
-
 </head>
 <body>
 
 
-
+<?php include_once '/incl/footerJs.php'; ?>
 </body>
 </html>
