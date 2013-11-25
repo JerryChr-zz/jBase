8f5608c3ed3102f2a5cf93dd2050f662a782d93e
Jerri Christiansen <jerri@redweb.dk>
2013-11-25 14:25:16 +0100

    Removing magic.css - update Bootstrap to v.3. updating template xml file

--- incl/footerJs.php
+++ incl/footerJs.php
@@ -1,4 +1,4 @@
 <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery-1.10.2.min.js" type="text/javascript"></script>
 <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/modernizr.min.js" type="text/javascript"></script>
 <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/scripts.js" type="text/javascript"></script>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 defined('_JEXEC') or die('Restricted access');
 ?>
 <?php echo '<?xml version="1.0" encoding="utf-8"?'.'>'; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
 <head>
 <!--
@@ -13,24 +13,24 @@ defined('_JEXEC') or die('Restricted access');
 	## Danmark                              ##
 	## email@redweb.dk                      ##
 	## http://www.redweb.dk                 ##
-	## Dato: 2013-07-09                     ##
+	## Dato: 2013-11-25                     ##
 	##########################################
 -->
 	<?php $this->setMetaData('generator','redWEB ApS – www.redweb.dk'); ?>
-
     <jdoc:include type="head" />
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/normalize.css" />
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/magic.css" />
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" />
     <link type="text/css" rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/lessphp/compile.php" />
+    <!--[if lt IE 9]>
+    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/html5shiv.js" type="text/javascript"></script>
+    <![endif]-->
 
 </head>
 <body>
 
 <h1>redBASE - Testing LESS PHP compiler - It's working if I have a red background</h1>
+
+
 
 
 <?php include_once 'incl/footerJs.php'; ?>
